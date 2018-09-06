07fbfd19926eed8c0f95f66406a7e0ad055287a5
sairoutine <sairoutine@gmail.com>
2018-09-07 04:32:34 +0900

    add: assets

---
+++ src/js/assets.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var AssetsConfig = {};
+AssetsConfig.images = {
+	//https://www.pixiv.net/member_illust.php?mode=medium&illust_id=37556413
+	"caution": "./image/caution.png",
+};
+
+AssetsConfig.sounds = {
+};
+
+AssetsConfig.bgms = {
+};
+
+
+module.exports = AssetsConfig;
