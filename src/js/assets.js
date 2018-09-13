3462a2f5e7f9abcd634ab7f120c8e8b9712a52dc
sairoutine <sairoutine@gmail.com>
2018-09-13 22:28:46 +0900

    add: images

--- src/js/assets.js
+++ src/js/assets.js
@@ -4,12 +4,28 @@ var AssetsConfig = {};
 AssetsConfig.images = {
 	//https://www.pixiv.net/member_illust.php?mode=medium&illust_id=37556413
 	"caution": "./image/caution.png",
+	//"trump": "./image/trump.gif",
+	//http://sozai.akuseru-design.com/category/sozai/button/
+	"button_white": "./image/button_white.png",
+	"button_gray": "./image/button_gray.png",
+	// http://www.pasocafe.com/balloon/balloon_01.php
+	"balloon_left": "./image/balloon_left.png",
+	"balloon_right": "./image/balloon_right.png",
 };
 
 AssetsConfig.sounds = {
 };
 
 AssetsConfig.bgms = {
+};
+
+AssetsConfig.fonts = {
+	// ロゴたいぷゴシックコンデンスド
+	// http://www.fontna.com/%E3%83%AD%E3%82%B4%E3%81%9F%E3%81%84%E3%81%B7%E3%82%B4%E3%82%B7%E3%83%83%E3%82%AF%E3%82%B3%E3%83%B3%E3%83%87%E3%83%B3%E3%82%B9%E3%83%89/
+	"MyFont": {
+		path: "07LogoTypeGothic-Condense.ttf",
+		format: "truetype",
+	},
 };
 
 
