37a5137a1e7c8836f7d801c59b1594ab0a5cd7b6
sairoutine <sairoutine@gmail.com>
2018-09-16 04:08:10 +0900

    update: show gun in lose class

--- src/js/scene/duel/lose.js
+++ src/js/scene/duel/lose.js
@@ -9,11 +9,17 @@ var SceneDuelLose = function(core) {
 	BaseScene.apply(this, arguments);
 	this._judge_message = new JudgeMessage(this);
 	this.addObjects([this._judge_message]);
+
+	this._gun_transparent = 0.0;
+	this._is_start_gun_extinguish = false;
 };
 Util.inherit(SceneDuelLose, BaseScene);
 
 SceneDuelLose.prototype.init = function(){
 	BaseScene.prototype.init.apply(this, arguments);
+
+	this._gun_transparent = 0.0;
+	this._is_start_gun_extinguish = false;
 
 	// トップを表に
 	this.parent.deck().topCard().flip();
@@ -26,6 +32,15 @@ SceneDuelLose.prototype.init = function(){
 SceneDuelLose.prototype.beforeDraw = function(){
 	BaseScene.prototype.beforeDraw.apply(this, arguments);
 
+	if (this._is_start_gun_extinguish && this._gun_transparent !== 0.0) {
+		this._gun_transparent -= 0.1;
+
+		if (this._gun_transparent <= 0.0) {
+			this._is_start_gun_extinguish = false;
+			this._gun_transparent = 0.0;
+		}
+	}
+
 	// N秒間は表にしたカードをその場所で見せ続ける
 	if(this.frame_count < 60) {
 		return;
@@ -33,6 +48,23 @@ SceneDuelLose.prototype.beforeDraw = function(){
 	else if (this.frame_count === 60) {
 		this.parent.startSerifExtinguish();
 		this._judge_message.extinguish();
+
+		this._showGun();
+		return;
+	}
+	// N秒間は銃を見せ続ける
+	else if(this.frame_count < 120) {
+		return;
+	}
+	else if (this.frame_count === 120) {
+		// ロシアンルーレット
+		if (this.parent.rule_manager.roulette()) {
+			// lose シーンはここで終わり
+			this.parent.changeSubScene("dead");
+		}
+		else {
+			this._hideGun();
+		}
 		return;
 	}
 	else {
@@ -54,6 +86,27 @@ SceneDuelLose.prototype.beforeDraw = function(){
 
 SceneDuelLose.prototype.draw = function(){
 	BaseScene.prototype.draw.apply(this, arguments);
+
+	if (this._gun_transparent === 0.0) {
+		return;
+	}
+
+	// TODO:
+	var ctx = this.core.ctx;
+	ctx.save();
+	ctx.globalAlpha = this._gun_transparent;
+	ctx.fillStyle = "red";
+	ctx.font = "48px 'MyFont'";
+	ctx.fillText("銃サンプル", 400, 100);
+	ctx.restore();
+};
+
+SceneDuelLose.prototype._showGun = function(){
+	this._gun_transparent = 1.0;
+};
+
+SceneDuelLose.prototype._hideGun = function(){
+	this._is_start_gun_extinguish = true;
 };
 
 module.exports = SceneDuelLose;
