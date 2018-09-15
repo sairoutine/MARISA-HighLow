8cc88cd8d6f61957c3cf31d55f26addc1f25a099
sairoutine <sairoutine@gmail.com>
2018-09-16 03:04:26 +0900

    implement: win sub scene

---
+++ src/js/object/judge_message.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var BaseObject = require('../hakurei').Object.Base;
+var Util = require('../hakurei').Util;
+
+var JudgeMessage = function(scene) {
+	BaseObject.apply(this, arguments);
+
+	this._text = "";
+	this._color = null;
+	this._transparent = 0.0;
+	this._is_start_extinguish = false;
+};
+Util.inherit(JudgeMessage, BaseObject);
+
+JudgeMessage.prototype.init = function(){
+	BaseObject.prototype.init.apply(this, arguments);
+
+	this._text = "";
+	this._color = "white";
+	this._transparent = 0.0;
+	this._is_start_extinguish = false;
+};
+
+JudgeMessage.prototype.beforeDraw = function(){
+	BaseObject.prototype.beforeDraw.apply(this, arguments);
+
+	if (this._is_start_extinguish && this._transparent !== 0.0) {
+		this._transparent -= 0.1;
+
+		if (this._transparent <= 0.0) {
+			this._is_start_extinguish = false;
+			this._transparent = 0.0;
+		}
+	}
+};
+
+JudgeMessage.prototype.draw = function(){
+	BaseObject.prototype.draw.apply(this, arguments);
+
+	if (this._transparent === 0.0) {
+		return;
+	}
+
+	var ctx = this.core.ctx;
+	ctx.save();
+	ctx.globalAlpha = this._transparent;
+	ctx.fillStyle = this._color;
+	ctx.font = "64px 'MyFont'";
+	ctx.textAlign = 'left';
+	ctx.textBaseAlign = 'top';
+	ctx.fillText(this._text, 170, 500);
+	ctx.restore();
+
+};
+
+JudgeMessage.prototype.show = function(text, color){
+	this._transparent = 1.0;
+	this._text = text;
+	this._color = color;
+};
+
+JudgeMessage.prototype.startExtinguish = function(){
+	this._is_start_extinguish = true;
+};
+
+
+
+module.exports = JudgeMessage;
